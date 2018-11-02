1a7c38cf2f0bf979c8bdddcc62e8cf6cbbc5eb29
qq1311971563 <1311971563@qq.com2>
2018-11-03 07:20:25 +0800

    更新至第八题。

--- src/main/java/lanqiao/seventh/GroupC.java
+++ src/main/java/lanqiao/seventh/GroupC.java
@@ -2,10 +2,54 @@ package lanqiao.seventh;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.Test;
 
 public class GroupC {
+
+	/**
+	 * @Description: 冰雹数
+	 * 
+	 *               任意给定一个正整数N， 如果是偶数，执行： N / 2 如果是奇数，执行： N * 3 + 1
+	 * 
+	 *               生成的新的数字再执行同样的动作，循环往复。
+	 * 
+	 *               通过观察发现，这个数字会一会儿上升到很高， 一会儿又降落下来。 就这样起起落落的，但最终必会落到“1”
+	 *               这有点像小冰雹粒子在冰雹云中翻滚增长的样子。
+	 * 
+	 *               比如N=9 9,28,14,7,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1
+	 *               可以看到，N=9的时候，这个“小冰雹”最高冲到了52这个高度。
+	 * 
+	 *               输入格式： 一个正整数N（N<1000000） 输出格式：
+	 *               一个正整数，表示不大于N的数字，经过冰雹数变换过程中，最高冲到了多少。
+	 * 
+	 *               例如，输入： 10 程序应该输出： 52
+	 * 
+	 *               再例如，输入： 100 程序应该输出： 9232
+	 * 
+	 *               资源约定： 峰值内存消耗（含虚拟机） < 256M CPU消耗 < 1000ms
+	 * 
+	 * @return: 这道题考语文的吧
+	 */
+	@Test
+	public void questionEight() {
+		int N, max = 0;
+		sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			int temp = i;
+			do {
+				max = max > temp ? max : temp;
+				if (temp % 2 == 0) {
+					temp = temp / 2;
+				} else {
+					temp = temp * 3 + 1;
+				}
+			} while (temp != 1);
+		}
+		System.out.println(max);
+	}
 
 	/**
 	 * @Description: 寒假作业
@@ -120,6 +164,7 @@ public class GroupC {
 	static boolean k6[] = new boolean[10];// 判断这个数有没有被取过
 	static int g6[] = new int[10];// 保存9个数
 	static int count = 0;
+	private Scanner sc;
 
 	/**
 	 * @Description: 分小组
